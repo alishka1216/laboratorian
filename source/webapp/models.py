7c6cb07a9c309dd7d1851959dec3a13e4de725d6
ALisher <alishka1216@gmail.com>
2021-03-05 16:12:47 +0600

    создал модели view urls

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+category_choices = [('technical', 'техника'), ('product', 'продукты'), ('things', 'вещи')]
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    description = models.CharField(max_length=3000, null=True, blank=True)
+    category = models.CharField(max_length=200, null=True, blank=True, choices=category_choices)
+    price.ost = models.IntegerField(min_value=0)
+    price = models.DecimalField()
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index_view(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', context={'products': products})
